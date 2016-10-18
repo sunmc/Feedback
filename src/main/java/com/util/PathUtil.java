5ed5634d60bba86080ac2d02dd4e9f27bb3e73b7
sxwzjps <sxwzjps@163.com>
2016-10-18 08:44:13 +0800

    1.引入jquery1.8 修复不能获取子元素数量的bug 2.修改界面显示内容，模块化，内容不会过长页面显示不全

--- src/main/java/com/util/PathUtil.java
+++ src/main/java/com/util/PathUtil.java
@@ -5,8 +5,8 @@ import javax.servlet.http.HttpServletRequest;
 public class PathUtil {
 
 	//文件上传存储路径
-	public static final String UP_LOAD_PATH = "D:/03 项目工程/03 eclipse/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Feedback/resource/images/";
-//	public static final String UP_LOAD_PATH = "F:/zq/resource";
+//	public static final String UP_LOAD_PATH = "D:/03 项目工程/03 eclipse/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/Feedback/resource/images/";
+	public static final String UP_LOAD_PATH = "D:/Feedbackupload/resources/";
 	public static String getBasePath(HttpServletRequest request){
 		String path = request.getContextPath();
 		String basePath = request.getScheme() + "://" + request.getServerName() + ":"
